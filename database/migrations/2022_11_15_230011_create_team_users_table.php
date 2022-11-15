63ce0597edd25be31475ffced37bbcf0d8b80bc7
nitishk@instaacoders.com <nitishk@instaacoders.com>
2022-11-15 23:46:38 +0530

    added user's realted migration and model

---
+++ database/migrations/2022_11_15_230011_create_team_users_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('team_users', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('team_id')->references('id')->on('teams')->cascadeOnDelete();
+            $table->foreignId('user_id')->references('id')->on('users')->cascadeOnDelete();
+            $table->timestamp('valid_from')->nullable();
+            $table->timestamp('valid_to')->nullable();
+
+            // OTHER COLUMNS DEFINATION
+
+            $table->integer('is_active')->default('1')->comment('1[Active] 2[Inactive] 3[Deleted]');
+            $table->string('status')->nullable();
+            $table->unsignedBigInteger('createdby_userid')->nullable();
+            $table->unsignedBigInteger('updatedby_userid')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('team_users');
+    }
+};
