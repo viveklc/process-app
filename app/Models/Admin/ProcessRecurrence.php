7ed52d8bcacaea4bb070baa55ae5886e061bf736
Praveen Kumar <praveensilaych@gmail.com>
2022-11-16 12:27:46 +0530

    Create Migration and Models as given below: 1. Process 2. ProcessDetail 3. ProcessRecurrence 4. ProcessTeam 5. Step

---
+++ app/Models/Admin/ProcessRecurrence.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Models\Admin;
+
+use App\Traits\CreatedUpdatedBy;
+use App\Traits\ModelScopes;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Spatie\Activitylog\Traits\LogsActivity;
+use Spatie\Activitylog\LogOptions;
+use App\Models\Admin\Process;
+
+class ProcessRecurrence extends Model
+{
+    use HasFactory, LogsActivity;
+    use CreatedUpdatedBy, ModelScopes;
+
+    protected $guarded = [];
+
+    /** Spatie Activity Log */
+    public function getActivitylogOptions(): LogOptions // spatie model log options
+    {
+        return LogOptions::defaults()->logAll()->useLogName('ProcessRecurrence');
+    }
+
+    /** Relationship */
+    public function process()
+    {
+        return $this->belongsTo(Process::class);
+    }
+
+}
